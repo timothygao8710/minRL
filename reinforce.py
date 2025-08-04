bc6e372b6a5a336624792d3624bcca99748f904c
Timothy Gao <timothygao@berkeley.edu>
2025-08-04 08:04:32 +0000

    runs

--- reinforce.py
+++ reinforce.py
@@ -29,14 +29,16 @@ wandb.init(
       "learning_rate": 1e-6,
       "temperature": 1,
       "beta": 0.9,
+      
+      "record_freq": 1000,
     }
 )
 
 config = wandb.config
 
 @jax.jit
-def preprocess(img_np: np.ndarray, out: int = 64) -> jnp.ndarray:
-    gray = np.dot(img_np[..., :3], [0.299, 0.587, 0.114])
+def preprocess(img_np, out: int = 64):
+    gray = jnp.dot(img_np[..., :3], jnp.array([0.299, 0.587, 0.114]))
     gray = jnp.asarray(gray, dtype=jnp.float32) / 255.0
     gray = jimage.resize(gray, (out, out), method="linear")
     return gray
@@ -55,11 +57,11 @@ class PI(nn.Module):
             x = nn.Dense(h)(x); x = nn.gelu(x)
         return nn.Dense(2)(x)
 
-def baseline(states, extra_info):
+def baseline(states, extra_info=None):
     return np.average(extra_info['rewards']) # a better baseline is previous pole angle
 
-def compute_adv(rewards, extra_info):
-    beta = config.beta # reduces variance at cost of bias
+def compute_adv(rewards, extra_info=None):
+    beta = config.beta # reduces variance at cost of bias (TODO: GAE)
     
     for i in range(len(rewards)-2, -1, -1): # reward to-go
         rewards[i] += beta * rewards[i+1]
@@ -104,47 +106,48 @@ def rollout(key, env):
     return {"states": S, "actions": A, "rewards" : R}
         
 @jax.jit
-def loss(states, acts, adv, model, params):
-    states, acts, adv = jnp.asarray(states), jnp.asarray(acts), jnp.asarray(adv) # convert to jnp arrays
-    logits = model.apply(params, states) # pi(s)
+def loss(params, states, acts, adv):
+    logits = pi.apply(params, states) # pi(s)
     log_probs = nn.log_softmax(logits) # pi(s) -> log p(*|s)
     log_probs = jnp.take_along_axis(log_probs, acts[:, None], axis=1).squeeze() # log p(*|s) -> log p(a|s)
     res = log_probs * adv # log p(a|s) -> adv * log p(a|s)
     return jnp.mean(res) # 1/N * sum(adv * log p(a|s))
 
-
 if __name__ == '__main__':
     env = gym.make("CartPole-v1", render_mode="rgb_array")
+    env = RecordVideo(env,
+        video_folder="/home/timothygao/minRL/videos",
+        name_prefix="rollout",
+        step_trigger=lambda step: step % config.record_freq == 0
+    )
     key = jax.random.PRNGKey(SEED)
     env.reset(seed=SEED)
     pi = PI()
     params = pi.init(key, preprocess(env.render())[None, :])
     lr = config.learning_rate
 
-    dloss_dparams = jax.grad(loss, argnums=4)
+    dloss_dparams = jax.grad(loss, argnums=0)
 
     # grad_params E_traj[return] = E_traj[grad_params log P(traj) * return] 
     # --> E_step[grad_params log P(a | s) * return] --> E_step[grad_params log P(a | s) * adv] (rewards to-go + baseline)
     for epoch in trange(1, config.epochs + 1):
-        S, A, R = [], []
+        S, A, R = [], [], []
         
         # 1) trajectory monte carlo sampling
-        for sample in range(1, config.rollouts_per_epoch + 1): 
+        for sample in trange(1, config.rollouts_per_epoch + 1): 
             episode = rollout(key, env)
             key = nxt_key(key)
             episode['rewards'] = compute_adv(episode['rewards'])
             S.extend(episode['states']) # retrieve all images from state
             A.extend(episode['actions'])
             R.extend(episode['rewards'])
-            
-            if (epoch * config.rollouts_per_epoch + sample) % 100 == 0:
-                video = np.stack(episode['states']).astype(np.uint8)
-                wandb.log({"rollout_video": wandb.Video(video, fps=30, format="mp4")}, step=epoch)
+        
+        S, A, R = jnp.asarray(S), jnp.asarray(A), jnp.asarray(R) # convert to jnp arrays
         
         # 2) update policy
         adv = R - baseline(S, {"rewards":R}) # substract mean reward from episode
         
-        grad = dloss_dparams(S, A, adv, pi, params)
+        grad = dloss_dparams(params, S, A, adv)
         
         params = jax.tree_util.tree_map(lambda u, v: u + lr * v, params, grad) # take direction of gradient step to maximize loss
         
@@ -152,8 +155,8 @@ if __name__ == '__main__':
         stats = {
             "alive_time": len(S) / config.rollouts_per_epoch,
             "avg_reward": np.mean(R),
-            "var_reward": np.variance(R),
-            "left_percentage" : A.count(0) / len(A),
+            "var_reward": np.var(R),
+            "left_percentage" : jnp.sum(A == 0) / len(A),
             "epoch": epoch,
         }
         wandb.log(stats, step=epoch)
